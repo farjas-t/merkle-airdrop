56208e989e64d7960a186cc99955f973e753f596
Farjas T <farjasktr@gmail.com>
2026-02-19 14:42:47 +0530

    burnable token

--- contracts/CSTToken.sol
+++ contracts/CSTToken.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CSTToken is ERC20, Ownable {
+contract CSTToken is ERC20, ERC20Burnable, Ownable {
     /**
      * @param initialSupply Total supply (in smallest units, e.g. with 18 decimals)
      * @param owner Address that will receive the initial supply
